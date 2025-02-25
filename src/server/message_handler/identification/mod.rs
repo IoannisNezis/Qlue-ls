59c341b39645150b68a66346a0b29f5b25e1c542
Julian Mundhahs <julian.mundhahs@gmail.com>
2025-02-25 22:30:59 +0100

    update for downstream changes

--- src/server/message_handler/identification/mod.rs
+++ src/server/message_handler/identification/mod.rs
@@ -1,6 +1,10 @@
 use tree_sitter::Parser;
 use wasm_bindgen::prelude::wasm_bindgen;
 
+
+// FIXME: This should not be exposed to Wasm API
+// This is a dirty hack to get things done in QLever.
+// QLever UI will implement a LSP-Client soon^TM.
 #[wasm_bindgen]
 pub fn determine_operation_type(text: String) -> Result<String, String> {
     let mut parser = Parser::new();
@@ -8,19 +12,16 @@ pub fn determine_operation_type(text: String) -> Result<String, String> {
         Ok(()) => {
             let tree = parser
                 .parse(text.as_bytes(), None)
-                .expect("Could not parse input");
+                .ok_or("Could not parse input")?;
             let root_node = tree.root_node();
             let mut cursor = root_node.walk();
             for op in root_node.children(&mut cursor) {
                 match op.kind() {
-                    "SelectQuery" | "ConstructQuery" | "DescribeQuery" | "AskQuery" => {
-                        return Ok("Query".into())
-                    }
+                    "Query" => return Ok("Query".into()),
                     "Update" => return Ok("Update".into()),
                     _ => {}
                 }
             }
-
             Ok("Unknown".into())
         }
         Err(e) => Err(format!("Could not set up parser: {}", e)),

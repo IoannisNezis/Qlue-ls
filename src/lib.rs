f25621a4b6b84c0a8a5e497bd0321b40bf303d34
Ioannis Nezis <ioannis@nezis.de>
2025-04-11 23:43:14 +0200

    MAINT: ser console error panic hook

--- src/lib.rs
+++ src/lib.rs
@@ -16,6 +16,8 @@ fn send_message(writer: &web_sys::WritableStreamDefaultWriter, message: String) 
 pub fn init_language_server(writer: web_sys::WritableStreamDefaultWriter) -> Server {
     #[cfg(target_arch = "wasm32")]
     wasm_logger::init(wasm_logger::Config::default());
+    #[cfg(target_arch = "wasm32")]
+    console_error_panic_hook::set_once();
     Server::new(move |message| send_message(&writer, message))
 }
 

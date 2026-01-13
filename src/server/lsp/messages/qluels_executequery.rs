bce5772dc9eddd32aa7df5933d938751b31bda32
Mark Grey <mgthesecond@spotify.com>
2026-01-13 16:46:37 -0500

    FEAT: listBackends method

---
+++ src/server/lsp/messages/qluels_executequery.rs
@@ -0,0 +1,45 @@
+use std::collections::HashMap;
+
+use serde::{Deserialize, Serialize};
+
+use crate::server::{
+    configuration::{CompletionTemplate, RequestMethod},
+    lsp::{rpc::NotificationMessageBase, LspMessage, NotificationMarker},
+};
+
+#[derive(Debug, Deserialize, PartialEq)]
+pub struct ExecuteQueryNotification {
+    #[serde(flatten)]
+    pub base: NotificationMessageBase,
+    pub params: SetBackendParams,
+}
+
+impl LspMessage for AddBackendNotification {
+    type Kind = NotificationMarker;
+
+    fn method(&self) -> Option<&str> {
+        Some("qlueLs/addBackend")
+    }
+
+    fn id(&self) -> Option<&crate::server::lsp::rpc::RequestId> {
+        None
+    }
+}
+
+#[derive(Debug, Deserialize, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct SetBackendParams {
+    pub service: BackendService,
+    pub request_method: Option<RequestMethod>,
+    pub default: bool,
+    pub prefix_map: Option<HashMap<String, String>>,
+    pub queries: Option<HashMap<CompletionTemplate, String>>,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct BackendService {
+    pub name: String,
+    pub url: String,
+    pub health_check_url: Option<String>,
+}

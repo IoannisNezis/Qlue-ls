f3177146c94a5660ac594f68c873b0ffcb8e4484
Ioannis Nezis <ioannis@nezis.de>
2026-02-19 18:34:00 +0100

    DEP: bumb ntparser to 2.0.0

--- src/server/sparql_operations/wasm.rs
+++ src/server/sparql_operations/wasm.rs
@@ -113,7 +113,7 @@ pub(crate) async fn execute_construct_query(
     .as_string()
     .unwrap();
     // Return the text as a JsValue
-    let triples = ntriples_parser::parse(text.as_bytes()).map_err(|_e| {
+    let (triples, _errors) = ntriples_parser::parse(text.as_bytes()).map_err(|_e| {
         SparqlRequestError::Deserialization("Could not read n-triples response".to_string())
     })?;
 
